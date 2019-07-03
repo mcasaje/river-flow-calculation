d30688469786f57ddef02068625479223711f0aa
Mark Casaje <mcasaje@gmail.com>
2019-07-03 16:38:03 -0700

    Implementation of SimpleAreaCalc.java

--- src/main/java/com/mcasaje/riverflowcalculation/entities/river/math/SimpleAreaCalc.java
+++ src/main/java/com/mcasaje/riverflowcalculation/entities/river/math/SimpleAreaCalc.java
@@ -6,7 +6,9 @@ class SimpleAreaCalc implements AreaCalc {
 
     @Override
     public BigDecimal calculateArea(BigDecimal width, BigDecimal depth) {
-        return null;
+        if (width == null) throw new IllegalArgumentException("Width cannot be null");
+        if (depth == null) throw new IllegalArgumentException("Depth cannot be null");
+        return width.multiply(depth);
     }
 
 }
