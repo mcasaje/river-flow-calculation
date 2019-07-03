87df0b985fd58202b0924265bce7e7b81c10bfd1
Mark Casaje <mcasaje@gmail.com>
2019-07-03 16:58:04 -0700

    Add Guice and created Guice module for AreaCalc

---
+++ src/main/java/com/mcasaje/riverflowcalculation/entities/river/math/area/AreaCalcGuiceModule.java
@@ -0,0 +1,11 @@
+package com.mcasaje.riverflowcalculation.entities.river.math.area;
+
+import com.google.inject.AbstractModule;
+
+public class AreaCalcGuiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(AreaCalc.class).to(SimpleAreaCalc.class);
+    }
+}
